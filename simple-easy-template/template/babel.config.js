1059dd7e43ee63c04c49d7b09c164984a011a225
xiaobiaobing <2412130200@qq.com>
2020-12-28 22:50:24 +0800

    feat:增加了部分文件夹和git提交校验

--- simple-easy-template/template/babel.config.js
+++ simple-easy-template/template/babel.config.js
@@ -1,25 +1,5 @@
-module.exports = function (api) {
-    if (api) {
-      api.cache.using(() => process.env.MODULES_ENV)
-    }
-    const modules = process.env.MODULES_ENV === 'lib' ? 'commonjs' : false
-    return {
-      presets: [
-        '@babel/preset-react',
-        [
-          '@babel/preset-env',
-          {
-            loose: true,
-            modules,
-          },
-        ]
-      ],
-      plugins: [
-        [
-          '@babel/plugin-transform-runtime'
-        ],
-        '@babel/plugin-transform-object-assign',
-        '@babel/plugin-proposal-class-properties'
-      ],
-    };
-  }
+module.exports = {
+  presets: [
+    ['@simple-easy/simple-easy-scripts/preset'],
+  ],
+};
