0769b21ebd1125cc828905be0eb05ca3f41dbd50
帅气的凯哥 <836133817@qq.com>
2021-02-23 22:47:34 +0800

    feat: 新增stylelint校验

---
+++ simple-easy-stylelint-config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+    "stylelint-config-prettier"
+  ],
+  "plugins": [
+    "stylelint-declaration-block-no-ignored-properties"
+  ],
+  "rules": {
+    "declaration-empty-line-before": "never"
+  }
+}
+  
